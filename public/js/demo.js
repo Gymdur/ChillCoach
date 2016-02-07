ee952871a8dd7f50ecfa2cd3656eb97f72f9d518
giodelatorre1 <giodelatorre1@gmail.com>
2016-02-07 03:10:37 -0500

    ugh

--- public/js/demo.js
+++ public/js/demo.js
@@ -90,6 +90,26 @@ $(document).ready(function() {
     $traits.hide();
     $results.hide();
 
+    
+// var text = $text.val();
+//$('.output-div')[0].scrollIntoView(true); // Boolean arguments
+//$.post('/tone', {'text': text })
+//.done(function(response) {
+// save the json response in the JSON tab
+//$jsonTab.html(JSON.stringify(response, null, 2));
+//prepare the data
+//processData(response);
+//response.id = 'root';
+//CURRENT_TONE = response;
+//doToneCheck(response, text);
+//})
+//.fail(onAPIError)
+//.always (function(){
+//$loading.hide();
+// scroll to bottom
+//$('.output-div')[0].scrollIntoView(true); // Boolean arguments
+//});
+
     $.ajax({
       headers:{
         'csrf-token': $('meta[name="ct"]').attr('content')
